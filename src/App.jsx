7a502b25f35a37303e18291dddafacb0311bb586
bagse <102260190+Bagse@users.noreply.github.com>
2023-02-21 18:17:23 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+import { MyRoutes } from "./routers/routes";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a>
+          <h1 className="title">+Movies 🎬</h1>
+        </a>
+      </header>
+      <MyRoutes />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/MovieDetails.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { get } from "../data/httpClient";
+import { getMovieImg } from "../utils/getMovieImg";
+import styles from "../pages/MovieDetails.module.css"
+
+export function MovieDetails() {
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState([]);
+  const [generos, setGeneros] = useState([]);
+
+  useEffect(() => {
+    get("/movie/" + movieId).then((data) => {
+      setMovie(data);
+      setGeneros(data.genres[0]);
+    });
+  }, [movieId]);
+
+  const imageUrl = getMovieImg(movie.poster_path, 500);
+
+  return (
+    <div className={styles.detailsContainer}>
+      <img src={imageUrl} alt={movie.title} className={`${styles.col} ${styles.movieImage}`} />
+      <div className={`${styles.col} ${styles.movieDetails}`}>
+        <p className={styles.title}>
+          {movie.title}
+        </p>
+        <p>
+          <strong>Genre: </strong>
+          {generos.name}
+        </p>
+        <p>
+          <strong>Description: </strong>
+          {movie.overview}
+        </p>
+      </div>
+    </div>
+  );
+}
