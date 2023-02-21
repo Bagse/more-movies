89208ce3f569435580221b633693528d8d21d988
bagse <102260190+Bagse@users.noreply.github.com>
2023-02-21 18:26:21 -0500

    gh pages uptade

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://Bagse.github.io/movies/",
+  base: "https://Bagse.github.io/more-movies/",
   plugins: [react()]
 })
